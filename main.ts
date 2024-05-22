7e5ec0687760cf81cc79a89fb0111b2cea26bc74
lancasterjoe <168177546+lancasterjoe@users.noreply.github.com>
2024-05-22 15:49:56 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,16 +18,28 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("" + (points))
 })
 radio.onReceivedValue(function (name, value) {
-    if (("A" as any) == ("B" as any) && ("B" as any) == ("C" as any)) {
+    if (name == "A") {
+        A = value
+    }
+    if (name == "B") {
+        B = value
+    }
+    if (name == "C") {
+        C = value
+    }
+    if (A == B && B == C) {
         basic.showString("+100")
         points += 100
-    } else if (("A" as any) == ("B" as any) || (("A" as any) == ("C" as any) || ("B" as any) == ("C" as any))) {
+    } else if (A == B && A != C || (A == C && A != B || B == C && A != B)) {
         basic.showString("+20")
         points += 20
     } else {
         basic.showIcon(IconNames.No)
     }
 })
+let C = 0
+let B = 0
+let A = 0
 let points = 0
 basic.showLeds(`
     # # # . .
@@ -37,4 +49,7 @@ basic.showLeds(`
     # . . . .
     `)
 points = 100
+A = 11
+B = 11
+C = 11
 radio.setGroup(1)
